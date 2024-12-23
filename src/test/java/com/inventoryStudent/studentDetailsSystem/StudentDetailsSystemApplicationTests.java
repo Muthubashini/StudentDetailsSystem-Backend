41cce8830290914e463bd08c4a23f655cf67fe53
Muthubashini <muthubashinisathsara.com>
2024-12-23 23:00:02 +0530

    created student details system backend part

---
+++ src/test/java/com/inventoryStudent/studentDetailsSystem/StudentDetailsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventoryStudent.studentDetailsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDetailsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
